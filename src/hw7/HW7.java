676448372b15fd22eef8a748a6e8cbf671dff455
rkaune <rkaune@ugcloud.ca>
2017-09-27 12:10:16 -0400

    Subtracted !!!!!!

--- src/hw7/HW7.java
+++ src/hw7/HW7.java
@@ -16,7 +16,7 @@ public class HW7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HW!!!!!!!!!!");
+        System.out.println("HW");
     }
     
 }
